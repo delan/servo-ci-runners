1af34c6a081f45539ca89d26cf4457a978272656
Delan Azabani <dazabani@igalia.com>
2025-10-30 18:21:00 +0800

    monitor: detect if blocked on permission prompt

--- monitor/src/utm.rs
+++ monitor/src/utm.rs
@@ -1,7 +1,8 @@
-use cmd_lib::run_cmd;
+use std::{process::Command, thread::sleep, time::Duration};
+
 use jane_eyre::eyre;
 use osakit::{self, declare_script};
-use tracing::error;
+use tracing::{error, warn};
 
 /// Trigger an automation permission prompt for UTM, on behalf of whatever context the monitor
 /// is running in (sshd-keygen-wrapper, Terminal, etc).
@@ -9,7 +10,25 @@ use tracing::error;
 /// Panics if UTM is not installed or someone chose to deny permission.
 pub fn request_automation_permission() -> eyre::Result<()> {
     // Not sure why the osakit crate can’t do this.
-    if run_cmd!(osascript -e r#"tell application "UTM""# -e "set vms to virtual machines" -e "end tell").is_err() {
+    let mut child = Command::new("osascript")
+        .args([
+            "-e",
+            r#"tell application "UTM""#,
+            "-e",
+            "set vms to virtual machines",
+            "-e",
+            "end tell",
+        ])
+        .spawn()?;
+    sleep(Duration::from_millis(250));
+    let status = loop {
+        match child.try_wait()? {
+            Some(status) => break status,
+            None => warn!("Waiting for permission prompt; please check the macOS UI"),
+        }
+        sleep(Duration::from_secs(10));
+    };
+    if !status.success() {
         error!("Failed to acquire automation permission for UTM!");
         error!("Either UTM is not installed, or someone chose to deny the permission.");
         error!("If UTM is installed, try clearing the automation permissions list:");
