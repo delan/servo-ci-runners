b36d5eddf03c3331fcc29f40e560ff6a97d77f4c
Delan Azabani <dazabani@igalia.com>
2025-10-30 15:17:07 +0800

    monitor: utm: initial clone_guest()

--- monitor/src/utm.rs
+++ monitor/src/utm.rs
@@ -1,2 +1,21 @@
-#[allow(unused)]
-use osakit;
+use jane_eyre::eyre;
+use osakit::{self, declare_script};
+
+pub fn clone_guest(original_guest_name: &str, new_guest_name: &str) -> eyre::Result<()> {
+    declare_script! {
+        #[language(AppleScript)]
+        #[source(r#"
+            on clone_guest(original_guest_name, new_guest_name)
+                tell application "UTM"
+                    set vm to virtual machine named original_guest_name
+                    duplicate vm with properties {configuration: {name: new_guest_name}}
+                end tell
+            end clone_guest
+        "#)]
+        Script {
+            fn clone_guest(original_guest_name: &str, new_guest_name: &str);
+        }
+    }
+    Script::new()?.clone_guest(original_guest_name, new_guest_name)?;
+    Ok(())
+}
--- monitor/src/utm_dummy.rs
+++ monitor/src/utm_dummy.rs
@@ -1,1 +1,5 @@
+use jane_eyre::eyre;
 
+pub fn clone_guest(original_guest_name: &str, new_guest_name: &str) -> eyre::Result<()> {
+    unimplemented!(r#"Requires `#[cfg(target_os = "macos")]`"#)
+}
